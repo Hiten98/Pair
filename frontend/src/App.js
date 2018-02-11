1731f6be2f79aafdfb797c55b178983a7f3fac9a
AdamKogut <akogut@purdue.edu>
2018-02-11 04:47:01 -0500

    Set up routing in top levels

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {NavLink, Switch, Route} from 'react-router-dom'
+import WelcomeScreen from './WelcomeScreen.js'
+import LandingScreen from './LandingScreen.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route path='/' component={WelcomeScreen}/>
+          <Route path='/home' component={LandingScreen}/>
+        </Switch>
       </div>
     );
   }
---
+++ frontend/src/WelcomeScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {NavLink, Switch, Route} from 'react-router-dom'
+//import './WelcomeScreen.css';
+
+class WelcomeScreen extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default WelcomeScreen;
