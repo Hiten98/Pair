ab6c7110e10ac94705ee3a666d1cbaa6f66615c5
AdamKogut <akogut@purdue.edu>
2018-02-11 06:32:17 -0500

    Fix password box compilation errors

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import {NavLink, Switch, Route} from 'react-router-dom'
+import {TextField} from 'material-ui'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import './Login.css';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import PasswordField from 'material-ui-password-field'
 
 class Login extends Component {
   render() {
     return (
-      <div>
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
         <div className="info">
         </div>
         <div className="loginForm">
+          <PasswordField
+            hintText="At least 8 characters"
+            floatingLabelText="Enter your password"
+            errorText="Your password is too short"/>
         </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
