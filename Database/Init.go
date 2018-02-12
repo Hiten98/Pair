5c585318b9e990e02a577271736e8c7afdfe9ecd
Darwin Vaz <dvaz@purdue.edu>
2018-02-11 19:52:17 -0500

    Setup database in server - code doesn't work

---
+++ Database/Init.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+
+ 	firebase "firebase.google.com/go"
+ 	google.golang.org/api/option"
+	
+	"golang.org/x/net/context"
+)
+
+func main() {
+	fmt.Println("Hello, playground")
+	opt := option.WithCredentialsFile("path/to/key.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	
+	client, err := app.Auth()
+	if err != nil {
+	  return err
+	}
+
+	decoded, err := client.VerifyIDToken(idToken)
+	uid := decoded.UID
+		
+}
