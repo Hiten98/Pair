0a10a60b6ca778056dd5f072e8cf2f700675657f
AdamKogut <akogut@purdue.edu>
2018-02-15 02:06:54 -0500

    Finish ui (except the paragraphs) for login page

--- frontend/src/form.js
+++ frontend/src/form.js
@@ -13,8 +13,12 @@ class form extends Component {
     password:null,
   }
 
-  getUserAndPass=(username,password)=>{
+  getUser=(username)=>{
     this.setState({username})
+    
+  }
+
+  getPass=(password)=>{
     this.setState({password})
   }
 
@@ -23,6 +27,8 @@ class form extends Component {
     const password=this.state.password
     //kunal code submit here
 
+    console.log(username)
+    console.log(password)
     //if doing a fetch use this at the end .then(this.redirect(classification))
     //where classification is whether it is an employee or intern, change the variable however you want
   }
@@ -44,13 +50,13 @@ class form extends Component {
             Login
         </Row>
 
-          <LoginFormField getUserAndPass={this.getUserAndPass}/>
+          <LoginFormField getUser={this.getUser} getPass={this.getPass}/>
 
           <Row className='row-sm lbutton'>
             <RaisedButton
               label="Login"
               primary={true}
-              onClick={this.handleLogin()}
+              onClick={this.handleLogin}
             />
           </Row>
 
--- frontend/src/loginFormField.js
+++ frontend/src/loginFormField.js
@@ -5,67 +5,70 @@ import PasswordField from 'material-ui-password-field'
 import { grey800, black } from 'material-ui/styles/colors';
 //import './Register.css';
 
-const styles = {
-  underlineStyle: {
-    borderColor: grey800,
-  },
-  floatingLabelStyle: {
-    color: grey800,
-  },
-  floatingLabelShrinkStyle: {
-    color: black,
-  },
-  hintStyle: {
-    color: grey800,
-    fontSize: '10vw',
-    textAlign: 'left',
-  },
-  visibilityIconStyle: {
-    opacity: '0',
+
+class loginFormField extends Component {
+  styles = {
+    underlineStyle: {
+      borderColor: grey800,
+    },
+    floatingLabelStyle: {
+      color: grey800,
+    },
+    floatingLabelShrinkStyle: {
+      color: black,
+    },
+    hintStyle: {
+      color: grey800,
+      fontSize: '10vw',
+      textAlign: 'left',
+    },
+    visibilityIconStyle: {
+      opacity: '0',
+    }
   }
-}
 
-const userChange=(ev)=>{
-//fill this in
-}
+  userChange = (ev) => {
+    this.props.getUser(ev.target.value)
+  }
 
-const passChange=(ev)=>{
-//fill this in
-}
+  passChange = (ev) => {
+    this.props.getPass(ev.target.value)
+  }
 
 
-const loginFormField = () => {
-  return (
-    <div>
-      <Row className="row-sm">
-        <PasswordField
-          className="username"
-          floatingLabelText="Enter your username"
-          visible
-          disableButton={true}
-          floatingLabelStyle={styles.floatingLabelStyle}
-          floatingLabelShrinkStyle={styles.floatingLabelShrinkStyle}
-          underlineStyle={styles.underlineStyle}
-          hintStyle={styles.hintStyle}
-          visibilityIconStyle={styles.visibilityIconStyle}
-          onChange={userChange()}
-        />
-      </Row>
-      <Row className="row-sm">
-        <PasswordField
-          className="password"
-          hintText="At least 8 characters"
-          floatingLabelText="Enter your password"
-          floatingLabelStyle={styles.floatingLabelStyle}
-          floatingLabelShrinkStyle={styles.floatingLabelShrinkStyle}
-          underlineStyle={styles.underlineStyle}
-          visibilityIconStyle={{ opacity: '0.8' }}
-          hintStyle={styles.hintStyle}
-          onChange={passChange()}
-        />
-      </Row>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Row className="row-sm">
+          <PasswordField
+            className="username"
+            floatingLabelText="Enter your username"
+            visible
+            disableButton={true}
+            floatingLabelStyle={this.styles.floatingLabelStyle}
+            floatingLabelShrinkStyle={this.styles.floatingLabelShrinkStyle}
+            underlineStyle={this.styles.underlineStyle}
+            hintStyle={this.styles.hintStyle}
+            visibilityIconStyle={this.styles.visibilityIconStyle}
+            onChange={this.userChange}
+          />
+        </Row>
+        <Row className="row-sm">
+          <PasswordField
+            className="password"
+            hintText="At least 8 characters"
+            floatingLabelText="Enter your password"
+            floatingLabelStyle={this.styles.floatingLabelStyle}
+            floatingLabelShrinkStyle={this.styles.floatingLabelShrinkStyle}
+            underlineStyle={this.styles.underlineStyle}
+            visibilityIconStyle={{ opacity: '0.8' }}
+            hintStyle={this.styles.hintStyle}
+            onChange={this.passChange}
+          />
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default loginFormField;
