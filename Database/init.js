072d5e52dac660f0b2b9e1d1459a74a8a0f32c5f
Darwin Vaz <dvaz@purdue.edu>
2018-02-16 12:10:51 -0500

    Split functions into relevant files

--- Database/init.js
+++ Database/init.js
@@ -1,57 +1,9 @@
+
       /*TODO @Darwin clean up and organize code
         TODO @Darwin move functions to appropriate place
         TODO @Darwin database security
         TODO @Darwin make separate javascript files*/
   
-
-    //All functions
-
-    /*
-    / @brief this function retrieves the already existent
-    /        items from a list and adds the new ones to it
-    /        can be used to update lists and arrays
-    /
-    / @param relevantRef a reference to the appropriate object header
-    / @param childName the name of the object you want to update
-    / @param itemName the item you want to update
-    / @param newValue the values you want to add
-    */
-    function getSnapshot(relevantRef, childName, itemName, newValue) {
-      var ref = relevantRef.child(childName).child(itemName)
-      var oldlist = [];
-      ref.once("value").then(function(snapshot) {
-        snapshot.forEach(function(childSnapshot) {
-          var item = childSnapshot.val();
-          oldlist.push(item);
-        });
-        var newList = oldlist.concat(newValue);
-        relevantRef.child(childName).update({
-          [itemName]: newList
-        })
-      });
-    }
-
-    function updateCompany(companyName, employees, locations = null) {
-      getSnapshot(companyRef, companyName, "listOfLocations", locations);
-      getSnapshot(companyRef, companyName, "listOfEmployees", employees);
-    };
-
-    function updateIntern(name, email, location) {
-      if(email != null) {
-        internRef.child(name).update({
-          "email": email
-        })
-      }
-      if(location != null) {
-        internRef.child(name).update({
-          "location": location
-        })
-      }
-    }
-
-
-
-
 
     //Initialize Firebase
     const config = {
@@ -77,9 +29,9 @@
     //updateCompany("Mind Readers Inc", ["France", "Trance", "Drance"], ["Person", "Persona"]);
     //updateCompany("Telefrags", [] , "Atlan");
 
-    internRef.update({
+    /*internRef.update({
       "AAlength": 0
-    });
+    });*/
 
     createIntern("Andrew", "$!@#$","bass1purduedu", "Hawaii");
     createIntern("Mihir", "paswrod", "tiwar@purdue.d", "NeYrok");
---
+++ Database/update.js
@@ -0,0 +1,43 @@
+	
+	/*
+    / @brief this function retrieves the already existent
+    /        items from a list and adds the new ones to it
+    /        can be used to update lists and arrays
+    /
+    / @param relevantRef a reference to the appropriate object header
+    / @param childName the name of the object you want to update
+    / @param itemName the item you want to update
+    / @param newValue the values you want to add
+    */
+    function getSnapshot(relevantRef, childName, itemName, newValue) {
+      var ref = relevantRef.child(childName).child(itemName)
+      var oldlist = [];
+      ref.once("value").then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var item = childSnapshot.val();
+          oldlist.push(item);
+        });
+        var newList = oldlist.concat(newValue);
+        relevantRef.child(childName).update({
+          [itemName]: newList
+        })
+      });
+    }
+
+    function updateCompany(companyName, employees, locations = null) {
+      getSnapshot(companyRef, companyName, "listOfLocations", locations);
+      getSnapshot(companyRef, companyName, "listOfEmployees", employees);
+    };
+
+    function updateIntern(name, email, location) {
+      if(email != null) {
+        internRef.child(name).update({
+          "email": email
+        })
+      }
+      if(location != null) {
+        internRef.child(name).update({
+          "location": location
+        })
+      }
+    }
