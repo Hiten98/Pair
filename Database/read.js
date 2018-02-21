638daf3b29a3e7d92191cacc5f145f1485f83122
Darwin Vaz <dvaz@purdue.edu>
2018-02-21 13:06:52 -0500

    Did MasterList & getIntern, need to return JSON

--- Database/read.js
+++ Database/read.js
@@ -1,12 +1,18 @@
 
-	function getMasterListOfInterns() {
+	function getMasterListOfInterns(company) {
+		var JSONArray = []
 		internRef.once("value").then(function(snapshot) {
-			document.write(snapshot.val());
-		});
+			snapshot.forEach(function(childSnapshot) {
+    			var item = childSnapshot.val().company;
+    			if(item == company)
+    				JSONArray.push(item);
+    		});
+    		// document.write(JSONArray);
+    	});
 	}
 
 	function getIntern(ID) {
-		internRef.child(ID).once.then(function(snapshot) {
+		internRef.child(ID).once("value").then(function(snapshot) {
 			document.write(snapshot.val());
 		});
 		//Figure out how to return a JSON
