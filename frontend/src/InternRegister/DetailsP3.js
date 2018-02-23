3ed7ce3dd5ae95fb163d17c19483e305eb814bd9
AdamKogut <akogut@purdue.edu>
2018-02-22 23:50:27 -0500

    Create housing preferences page

--- frontend/src/InternRegister/DetailsP3.js
+++ frontend/src/InternRegister/DetailsP3.js
@@ -1,23 +1,72 @@
 import React, { Component } from 'react';
 import { NavLink, Switch, Route } from 'react-router-dom'
 import { Grid, Row, Col, Image } from 'react-bootstrap'
-import HousingPreferences from './HousingPreferences.js'
 import DetailsStepper from './DetailsStepper'
+import { RaisedButton } from 'material-ui';
+import PriceInput from './HousingInputs/PriceInput'
+import DistanceInput from './HousingInputs/DistanceInput'
+import DurationInput from './HousingInputs/DurationInput'
+import RoommatesInput from './HousingInputs/RoommatesInput'
 
 import history from '../history'
 //import './LandingScreen.css';
 
 class DetailsP1 extends Component {
-  state={
-    price:null,
-    roommates:null,
-    distance:null,
-    duration:null,
-    changed:false,
+  state = {
+    price: null,
+    roommates: null,
+    distance: null,
+    duration: null,
+    changed: false,
   }
 
-  hasChanged=()=>{
-    return this.state.changed
+  componentWillMount() {
+    //KUNAL PUT CODE HERE to get preferences from server
+    //put them in the nulls that are below
+    this.setState({
+      price: null,
+      roommates: null,
+      distance: null,
+      duration: null,
+    })
+  }
+
+  bSubmit = () => {
+    if (this.state.changed) {
+      let price = this.state.price
+      let roommates = this.state.roommates
+      let distance = this.state.distance
+      let duration = this.state.duration
+
+      //KUNAL PUT CODE HERE to submit the page to the server
+      //dont forget to check that all are not null
+    }
+  }
+
+  distanceChange=(distance)=>{
+    this.setState({distance})
+  }
+
+  priceChange=(price)=>{
+    this.setState({price})
+  }
+
+  durationChange=(duration)=>{
+    this.setState({duration})
+  }
+
+  roommateChange=(roommates)=>{
+    this.setState({roommates})
+  }
+
+  backButtonSubmit = () => {
+    this.bSubmit()
+    history.push('/intern/roommate-preferences')
+  }
+
+  buttonSubmit = () => {
+    this.bSubmit()
+    history.push('/intern-landing/chats')
   }
 
   render() {
@@ -30,10 +79,39 @@ class DetailsP1 extends Component {
           <Col xs={10} sm={8} className="midCol">
             <Row className="Title">Housing Preferences</Row>
             <Row>
-              <DetailsStepper hasChanged={this.state.changed} pos={3}/>
+              <DetailsStepper hasChanged={this.state.changed} pos={3} />
             </Row>
             <Row>
-              <HousingPreferences />
+              <Col xs={6}>
+                <PriceInput dv={this.state.price} changing={this.priceChange.bind(this)} />
+              </Col>
+              <Col xs={6}>
+                <DistanceInput dv={this.state.distance} changing={this.distanceChange.bind(this)} />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={6}>
+                <DurationInput dv={this.state.price} changing={this.durationChange.bind(this)} />
+              </Col>
+              <Col xs={6}>
+                <RoommatesInput dv={this.state.distance} changing={this.roommateChange.bind(this)} />
+              </Col>
+            </Row>
+            <Row className="notThis">
+              <RaisedButton
+                label="Previous"
+                sytle={{ marginTop: "20px" }}
+                primary
+                onClick={this.backButtonSubmit}
+              />
+              <span className="hiddenText">Th</span>
+
+              <RaisedButton
+                label="Submit"
+                style={{ marginTop: "20px", }}
+                primary
+                onClick={this.buttonSubmit}
+              />
             </Row>
           </Col>
           <Col xs={1} sm={2}>
