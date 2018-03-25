6e9430b69a529a5fe06124f713aff48307eb827f
aswami98 <aswami@purdue.edu>
2018-03-24 21:41:23 -0400

    copied files that I changed to CompanyRegister folder

---
+++ frontend/src/Register/RegisterButtons copy.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Row } from 'react-bootstrap'
+import { RaisedButton } from 'material-ui'
+import history from '../history'
+import axios from 'axios'
+//import './RegisterButtons.css';
+
+axios.defaults.baseURL = 'http://localhost:9090'
+
+class RegisterButtons extends Component {
+
+
+  handleLogin = () => {
+    const companyCode = this.props.companyCode
+    let that = this
+
+    if (companyCode != null && companyCode.length == 4) {
+      axios.post('/GET-COMPANY', {
+        "pid": {companyCode}
+      }).then(function (response) {
+        //console.log(response.data);
+        if (response.data.status) {
+          //let parsed = JSON.parse(JSON.stringify(response.data.company))
+          let locat = []
+          that.props.updateCompany(response.data.name)
+          for (var loc in response.data.locations){
+            locat.push(response.data.locations[loc])
+            that.props.updateLocations(locat)
+          }
+          //if(co)
+          history.push('/register/employee')
+        } else {
+          alert('Invalid company code, please try again')
+        }
+      }).catch(function (error) {
+        console.log(error);
+      });
+
+    } else {
+      alert('Invalid company code, please try again')
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Row className='row-sm lbutton'>
+          <RaisedButton
+            label="Register"
+            primary={true}
+            onClick={this.handleLogin}
+          />
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default RegisterButtons;
