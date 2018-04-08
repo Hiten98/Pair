0985edab78c65eab2af7a8d082649930edf5d4b5
aswami98 <aswami@purdue.edu>
2018-04-08 17:42:04 -0400

    implemented notification popover

--- frontend/src/Landing/TopBar/Notifications.js
+++ frontend/src/Landing/TopBar/Notifications.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { NavLink, Switch, Route } from 'react-router-dom'
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 //import './Notifications.css';
 
 class Notifications extends Component {
@@ -7,10 +10,29 @@ class Notifications extends Component {
     super(props)
   }
 
+  componentDidMount=()=>{
+    console.log("Start!");
+    console.log(this.props);
+    console.log(this.props.uid);
+  }
+
   render() {
     return (
       <div>
-        {/* Arvindh code here */}
+      <Popover
+        open={this.props.notificationVisible}
+        anchorEl={this.props.anchorEl}
+        anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+        targetOrigin={{horizontal: 'left', vertical: 'top'}}
+        onRequestClose={this.props.closeNotifications}
+      >
+        <Menu>
+          <MenuItem primaryText="Notification 1" />
+          <MenuItem primaryText="Notification 2" />
+          <MenuItem primaryText="Notification 3" />
+          <MenuItem primaryText="Notification 4" />
+        </Menu>
+      </Popover>
       </div>
     );
   }
