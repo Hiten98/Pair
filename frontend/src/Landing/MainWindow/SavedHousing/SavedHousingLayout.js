8aff356497d6ec5fd84beb1b616be650e3d4dc64
Kunal Sinha <kunalsinha98@gmail.com>
2018-04-14 22:14:08 -0400

    Likes Work Now!

--- frontend/src/Landing/MainWindow/SavedHousing/SavedHousingLayout.js
+++ frontend/src/Landing/MainWindow/SavedHousing/SavedHousingLayout.js
@@ -43,29 +43,51 @@ class LandingScreen extends Component {
     };
   }
 
-  handleReviewText = (event, newValue) => {
-    this.setState({reviewText: newValue});
-  }
-
-  handleAddReview = (address) => {
-    this.setState({
-      selectedHouse: address
-    }, () => {
-      if(address != "" && this.state.reviewText != "") {
-        axios.post("/WRITE-REVIEW", {
-          house: address,
-          review: this.state.reviewText
-        }).then((response) => {
+  handleLike = (address, chatName) => {
+    this.setState({ selectedHouse: address }, () => {
+      axios
+        .post("/LIKE-HOUSE", {
+          userID: this.props.uid,
+          name: chatName,
+          house: address
+        })
+        .then(response => {
           console.log(response.data);
-          this.render(); 
-        }).catch((error) => {
+          this.getHousing(this.props);
+
+        })
+        .catch(error => {
           console.log(error);
         });
+    });
+  };
+
+  handleReviewText = (event, newValue) => {
+    this.setState({ reviewText: newValue });
+  };
+
+  handleAddReview = address => {
+    this.setState(
+      {
+        selectedHouse: address
+      },
+      () => {
+        if (address != "" && this.state.reviewText != "") {
+          axios
+            .post("/WRITE-REVIEW", {
+              house: address,
+              review: this.state.reviewText
+            })
+            .then(response => {
+              this.render();
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        }
       }
-      // console.log(address+this.state.reviewText)
-    }
     );
-  }
+  };
 
   handleURL = url => {
     window.open(url, "_blank");
@@ -82,7 +104,6 @@ class LandingScreen extends Component {
       })
       .then(function(response) {
         // Make Cards for House Listings
-        // console.log(response.data);
         let r = [];
         for (let ind in response.data) {
           if (response.data[ind][0] == "3") {
@@ -113,19 +134,16 @@ class LandingScreen extends Component {
 
   handleCloseDialogWithSubmit = () => {
     this.setState({ openDialog: false });
-    console.log(this.state.radioValue)
-    console.log(this.props.uid)
-    console.log(this.state.selectedHouse)
     axios
       .post("/ADD-HOUSE", {
         name: this.state.radioValue,
         userID: this.props.uid,
         house: this.state.selectedHouse
       })
-      .then((response) => {
+      .then(response => {
         console.log(response);
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
       });
   };
@@ -144,131 +162,105 @@ class LandingScreen extends Component {
     this.setState({ open: false });
   };
 
-  minBedSlider = (event, value) => {
-    this.setState({ minBed: value });
+  componentWillReceiveProps = nextProps => {
+    if (this.props.state.currChatName != nextProps.state.currChatName)
+      this.getHousing(nextProps);
   };
 
-  maxBedSlider = (event, value) => {
-    this.setState({ maxBed: value });
-  };
-
-  minBathSlider = (event, value) => {
-    this.setState({ minBath: value });
-  };
-
-  maxBathSlider = (event, value) => {
-    this.setState({ maxBath: value });
-  };
-
-  minPriceSlider = (event, value) => {
-    this.setState({ minPrice: value });
-  };
-
-  maxPriceSlider = (event, value) => {
-    this.setState({ maxPrice: value });
-  };
-
-  minSqFtSlider = (event, value) => {
-    this.setState({ minSqFt: value });
-  };
-
-  maxSqFtSlider = (event, value) => {
-    this.setState({ maxSqFt: value });
-  };
-
-  handleSearch = () => {
-    // console.log(this.props);
-    this.handleClose();
+  getHousing = props => {
     // Go back to first 10 or 20 houses when search is made again with new filters
-    this.setState({ offset: 0 });
+    this.setState({ offset: 0, houseCards: [] });
 
     // Server Call with housing filter parameters to get first 10 or 20 houses
     let that = this;
-    // console.log(this.state.offset)
+
     axios
-      .post("/GET-FILTERED-HOUSES", {
-        state: "CA",
-        offset: this.state.offset,
-        minBedrooms: this.state.minBed,
-        maxBedrooms: this.state.maxBed,
-        minBathrooms: this.state.minBath,
-        maxBathrooms: this.state.maxBath,
-        minPrice: this.state.minPrice,
-        maxPrice: this.state.maxPrice,
-        minsqft: this.state.minSqFt,
-        maxsqft: this.state.maxSqFt,
-        houses: this.state.houses
+      .post("/GET-SAVED-HOUSES", {
+        name: props.state.currChatName
       })
       .then(function(response) {
         // Make Cards for House Listings
-        // console.log(response.data);
         let tempCard = [];
+        console.log("SAVED HOUSES:");
+        console.log(response.data);
+        if (response.data.status != false) {
+          for (let i in response.data) {
+            let details = "";
+            let reviews = [];
+            if (
+              response.data[i].bedrooms > 0 &&
+              response.data[i].bedrooms != null
+            )
+              details += response.data[i].bedrooms + " Bed • ";
+            if (
+              response.data[i].bathrooms > 0 &&
+              response.data[i].bathrooms != null
+            )
+              details += response.data[i].bathrooms + " Bath • ";
+            if (response.data[i].sqft > 0 && response.data[i].sqft != null)
+              details += response.data[i].sqft + " sqft • ";
+            if (response.data[i].price > 0 && response.data[i].price != null)
+              details += "$" + response.data[i].price;
 
-        for (let i in response.data) {
-          if (!isNaN(response.data[i])) continue;
-          let details = "";
-          let reviews = [];
-          if (
-            response.data[i].bedrooms > 0 &&
-            response.data[i].bedrooms != null
-          )
-            details += response.data[i].bedrooms + " Bed • ";
-          if (
-            response.data[i].bathrooms > 0 &&
-            response.data[i].bathrooms != null
-          )
-            details += response.data[i].bathrooms + " Bath • ";
-          if (response.data[i].sqft > 0 && response.data[i].sqft != null)
-            details += response.data[i].sqft + " sqft • ";
-          if (response.data[i].price > 0 && response.data[i].price != null)
-            details += "$" + response.data[i].price;
+            for (let k in response.data[i].listOfReviews) {
+              reviews.push(
+                <Paper key={k}>
+                  <MenuItem primaryText={response.data[i].listOfReviews[k]} />
+                </Paper>
+              );
+            }
+            let str = "";
+            console.log(response.data[i]);
+            if (response.data[i].likes <= 0) {
+              str = "Likes (0)";
+            } else {
+              str = "Likes (" + response.data[i].likes + ")";
+            }
+            tempCard.push(
+              <Card>
+                <CardHeader
+                  title={i}
+                  subtitle={details}
+                  actAsExpander={true}
+                  showExpandableButton={true}
+                />
 
-          for (let k in response.data[i].listOfReviews)
-            reviews.push(
-              <Paper key={k}>
-                <MenuItem primaryText={response.data[i].listOfReviews[k]} />
-              </Paper>
+                <CardActions style={{ marginTop: "-25px" }}>
+                  <FlatButton
+                    label={str}
+                    secondary
+                    onClick={() => that.handleLike(i, props.state.currChatName)}
+                  />
+                  <FlatButton
+                    label="Go to Listing"
+                    secondary
+                    onClick={() => that.handleURL(response.data[i].url)}
+                  />
+                </CardActions>
+
+                <CardText expandable={true} style={{ marginTop: "-20px" }}>
+                  <h5>Reviews: </h5>
+                  {reviews.length > 1 ? reviews : <h5>No Reviews</h5>}
+                  <TextField
+                    hintText="Type Review Here"
+                    multiLine={true}
+                    rows={2}
+                    rowsMax={8}
+                    fullWidth={true}
+                    onChange={that.handleReviewText}
+                  />
+                </CardText>
+
+                <CardActions expandable style={{ marginTop: "-20px" }}>
+                  <FlatButton
+                    label="Add Review"
+                    secondary
+                    onClick={() => that.handleAddReview(i)}
+                  />
+                </CardActions>
+              </Card>
             );
-          tempCard.push(
-            <Card key={i}>
-              <CardHeader
-                title={response.data[i].address}
-                subtitle={details}
-                actAsExpander={true}
-                showExpandableButton={true}
-              />
-
-              <CardActions style={{ marginTop: "-25px" }}>
-                <FlatButton
-                  label="Save House"
-                  secondary
-                  onClick={() => that.handleSave(response.data[i].address)}
-                />
-                <FlatButton
-                  label="Go to Listing"
-                  secondary
-                  onClick={() => that.handleURL(response.data[i].url)}
-                />
-              </CardActions>
-
-               <CardText expandable={true} style={{ marginTop: "-20px"}}>
-                <h5>Reviews: </h5>
-                {reviews.length > 1 ? reviews : <h5>No Reviews</h5>}
-                <TextField 
-                hintText="Type Review Here" 
-                multiLine={true} 
-                rows={2}
-                rowsMax={8}
-                fullWidth={true}
-                onChange={that.handleReviewText}
-              />
-              </CardText>
-              
-              <CardActions expandable style={{ marginTop: "-20px" }}>
-                <FlatButton label="Add Review" secondary onClick={() => that.handleAddReview(response.data[i].address)}/>
-              </CardActions>
-            </Card>
-          );
+          }
         }
         that.setState({ houseCards: tempCard });
       })
@@ -278,104 +270,10 @@ class LandingScreen extends Component {
   };
 
   componentDidMount() {
-    // Go back to first 10 or 20 houses when search is made again with new filters
-    this.setState({ offset: 0 });
-
-    // Server Call with housing filter parameters to get first 10 or 20 houses
-    let that = this;
-    axios
-      .post("/GET-HOUSES", {
-        state: "CA",
-        offset: this.state.offset
-      })
-      .then(function(response) {
-        // Make Cards for House Listings
-        // console.log(response.data);
-        let tempCard = [];
-
-        for (let i in response.data) {
-          if (!isNaN(response.data[i])) continue;
-          let details = "";
-          let reviews = [];
-          if (
-            response.data[i].bedrooms > 0 &&
-            response.data[i].bedrooms != null
-          )
-            details += response.data[i].bedrooms + " Bed • ";
-          if (
-            response.data[i].bathrooms > 0 &&
-            response.data[i].bathrooms != null
-          )
-            details += response.data[i].bathrooms + " Bath • ";
-          if (response.data[i].sqft > 0 && response.data[i].sqft != null)
-            details += response.data[i].sqft + " sqft • ";
-          if (response.data[i].price > 0 && response.data[i].price != null)
-            details += "$" + response.data[i].price;
-
-          for (let k in response.data[i].listOfReviews)
-            reviews.push(
-              <Paper key={k}>
-                <MenuItem primaryText={response.data[i].listOfReviews[k]} />
-              </Paper>
-            );
-          tempCard.push(
-            <Card key={i}>
-              <CardHeader
-                title={response.data[i].address}
-                subtitle={details}
-                actAsExpander={true}
-                showExpandableButton={true}
-              />
-
-              <CardActions style={{ marginTop: "-25px" }}>
-                <FlatButton
-                  label="Save House"
-                  secondary
-                  onClick={() => that.handleSave(response.data[i].address)}
-                />
-                <FlatButton
-                  label="Go to Listing"
-                  secondary
-                  onClick={() => that.handleURL(response.data[i].url)}
-                />
-              </CardActions>
-
-              <CardText expandable={true} style={{ marginTop: "-20px"}}>
-                <h5>Reviews: </h5>
-                {reviews.length > 1 ? reviews : <h5>No Reviews</h5>}
-                <TextField 
-                hintText="Type Review Here" 
-                multiLine={true} 
-                rows={2}
-                rowsMax={8}
-                fullWidth={true}
-                onChange={that.handleReviewText}
-              />
-              </CardText>
-              
-              <CardActions expandable style={{ marginTop: "-20px" }}>
-                <FlatButton label="Add Review" secondary onClick={() => that.handleAddReview(response.data[i].address)}/>
-              </CardActions>
-            </Card>
-          );
-        }
-        that.setState({ houseCards: tempCard });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+    this.getHousing(this.props);
   }
 
   render() {
-    const actions = [
-      <FlatButton label="Cancel" primary={true} onClick={this.handleClose} />,
-      <FlatButton
-        label="Apply Filters"
-        primary={true}
-        onClick={this.handleSearch}
-      />
-    ];
-
     const actions2 = [
       <FlatButton
         label="Cancel"
@@ -391,89 +289,6 @@ class LandingScreen extends Component {
 
     return (
       <div style={{ textAlign: "left" }}>
-        <RaisedButton label="Filters" onClick={this.handleOpen} />
-        <Dialog
-          actions={actions}
-          autoScrollBodyContent
-          modal={true}
-          open={this.state.open}
-          onRequestClose={this.handleClose}
-        >
-          <div style={{ overflowY: "auto", overflowX: "hidden" }}>
-            <p>Minimum Number of Beds:&nbsp;{this.state.minBed}</p>
-            <Slider
-              min={0}
-              max={10}
-              step={1}
-              value={this.state.minBed}
-              onChange={this.minBedSlider}
-            />
-
-            <p>Maximum number of Beds:&nbsp;{this.state.maxBed}</p>
-            <Slider
-              min={0}
-              max={10}
-              step={1}
-              value={this.state.maxBed}
-              onChange={this.maxBedSlider}
-            />
-
-            <p>Minimum Number of Baths:&nbsp;{this.state.minBath}</p>
-            <Slider
-              min={0}
-              max={10}
-              step={1}
-              value={this.state.minBath}
-              onChange={this.minBathSlider}
-            />
-
-            <p>Maximum number of Baths:&nbsp;{this.state.maxBath}</p>
-            <Slider
-              min={0}
-              max={10}
-              step={1}
-              value={this.state.maxBath}
-              onChange={this.maxBathSlider}
-            />
-
-            <p>Minimum Price:&nbsp;{this.state.minPrice}</p>
-            <Slider
-              min={0}
-              max={5000}
-              step={100}
-              value={this.state.minPrice}
-              onChange={this.minPriceSlider}
-            />
-
-            <p>Maximum Price:&nbsp;{this.state.maxPrice}</p>
-            <Slider
-              min={0}
-              max={5000}
-              step={100}
-              value={this.state.maxPrice}
-              onChange={this.maxPriceSlider}
-            />
-
-            <p>Minimum Square Feet:&nbsp;{this.state.minSqFt}</p>
-            <Slider
-              min={0}
-              max={5000}
-              step={100}
-              value={this.state.minSqFt}
-              onChange={this.minSqFtSlider}
-            />
-
-            <p>Maximum Square Feet:&nbsp;{this.state.maxSqFt}</p>
-            <Slider
-              min={0}
-              max={5000}
-              step={100}
-              value={this.state.maxSqFt}
-              onChange={this.maxSqFtSlider}
-            />
-          </div>
-        </Dialog>
-
         {this.state.houseCards}
 
         <Dialog
